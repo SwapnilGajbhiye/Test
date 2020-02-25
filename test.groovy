f6b201ee7ae04cda4ddab530bfef5aa873f8c29b
Swapnil G2 <swapnil@SwapnilG2.local>
2020-02-26 04:24:20 +0530

    --- Auto-Git Commit ---

--- test.groovy
+++ test.groovy
@@ -3,7 +3,7 @@ pipeline {
         stages {
           stage('Build') { 
             steps {
-              print 
+              print ("Build ") 
             }
           }
           stage('Test') { 
